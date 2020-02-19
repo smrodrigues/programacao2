e03129fb6566c2c7185e81ac1be99eefedc95a5a
smrodrigues <48528984+smrodrigues@users.noreply.github.com>
2020-02-19 08:43:44 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,3 @@
 x = int (input ("digite um número:"))
+y = int (input ("digite um número:"))
 opcao = str (input ("a para adição" "\n s para subtração" "\n d para divisão" "\n m para multiplicação \n escolha:"))
